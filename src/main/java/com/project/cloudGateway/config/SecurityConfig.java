c202d3828328719fdc41b013a3bfee5681c3d3eb
Carina <carina_porumb@yahoo.com>
2023-05-24 17:12:57 +0300

    Initial commit

---
+++ src/main/java/com/project/cloudGateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.project.cloudGateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .oauth2ResourceServer(configurer -> configurer.jwt(jwtSpec -> {
+                }))
+                .build();
+
+    }
+}
